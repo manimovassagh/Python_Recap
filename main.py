972d7e9ef2e8fc5da8f1ff26469183de652b396b
manimovassagh <mani.mgh@gmail.com>
2023-02-10 11:55:47 +0100

    sec

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     print('Hi Mani from main func')
     secondary_function()
+    my_input = input('Give me some items mani! \n')
+    print('Nice to meet you ', my_input)
 
 
 def secondary_function():
     print('Hi from secondary function')
 
+
+seconds = 12
+minutes = seconds / 60
+print(minutes)
+# sample comment
 
 if __name__ == '__main__':
     main()
