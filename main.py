c678aac4f5efe43c13bb4640adaa8730c4197cf9
manimovassagh <mani.mgh@gmail.com>
2023-02-10 11:49:36 +0100

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print('Hi Mani from main func')
+    secondary_function()
+
+
+def secondary_function():
+    print('Hi from secondary function')
+
+
+if __name__ == '__main__':
+    main()
